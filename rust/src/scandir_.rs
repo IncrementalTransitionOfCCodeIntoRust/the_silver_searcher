41ef1e4f301366fc46c7a6afbd8df93e8c6173c9
Philip Koslowski <philip.koslowski@gmail.com>
2020-08-23 13:02:50 +0200

    working with concrete function instead of function as function parameter

--- rust/src/scandir_.rs
+++ rust/src/scandir_.rs
@@ -1,31 +1,16 @@
-extern crate libc;
-
-use crate::bindings::{ ignores, scandir_baton_t, filter_fp };
-use libc::{ dirent, opendir, readdir, closedir };
+use crate::bindings::{ dirent, opendir, readdir, readdir_r, closedir,
+    ignores, scandir_baton_t, filter_fp, filename_filter };
 
 use std::ffi::c_void;
 use std::mem;
-
-// pub unsafe extern "C" fn ag_scandir(
-//     dirname: *const libc::c_char,
-//     namelist: *mut *mut *mut dirent,
-//     mut f: filter_fp,
-//     baton: *mut libc::c_void)
-//     -> libc::c_int
-// {
-//     return call_mut(dirname, namelist, f, baton);
-// }
 
 #[no_mangle]
 pub unsafe extern "C" fn ag_scandir(
     dirname: *const cty::c_char,
     namelist: *mut *mut *mut dirent,
     f: filter_fp,
-    baton: *mut cty::c_void) -> cty::c_int
+    baton: *mut libc::c_void) -> cty::c_int
 {
-    //let names_len = 32;
-    //let mut results_len: cty::c_int = 0;
-
     let dirp = opendir(dirname);
     if dirp.is_null() {
         closedir(dirp);
@@ -38,23 +23,24 @@ pub unsafe extern "C" fn ag_scandir(
     }
 
     let mut names = Vec::new();
-
     loop {
         let mut entry = readdir(dirp);
         if entry.is_null() { break; }
 
-        // if f(dirname, entry, baton) == 0 {
-        //     continue;
-        // }
+        // TODO - check this one out, couple with this function's argument, get it to work ...
+        if filename_filter(dirname, entry, baton) == 0 {
+            continue;
+        }
 
-        //names[results_len as usize] = entry;
         names.push(entry);
-        //results_len += 1;
     }
 
     closedir(dirp);
+    names.shrink_to_fit();
+    assert!(names.len() == names.capacity());
     let names_len = names.len() as cty::c_int;
-    *namelist = names.into_raw_parts().0;
+    //*namelist = names.into_raw_parts().0;
+    *namelist = names.as_mut_ptr();
 
     return names_len;
 }
